ba9f792d507a4bf9b5f258fd71eee4a2060a7c61
jensen zhang <rasengun05@gmail.com>
2021-10-09 19:36:01 -0400

    base for website

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=jensenzhng&limit=1&api_key=a2fc7e8497efae9583ecda43732a8d14&format=json
+//found endpoint for spotify api
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
