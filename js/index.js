5a09ff70f0a3c88a0e4a3cace8198da0c40e17b5
jensen zhang <rasengun05@gmail.com>
2021-10-16 19:23:17 -0400

    revert to lastfm b/c discord is bugging out

--- js/index.js
+++ js/index.js
@@ -5,24 +5,27 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-let displayStatus = async (data) =>{
-  if (data.listening_to_spotify) {
-    let status = `Listening to ${data.spotify.song} by ${data.spotify.artist} on Spotify`;
+let getSpotifyStatus = async () =>{
+
+  let res = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=jensenzhng&limit=1&api_key=a2fc7e8497efae9583ecda43732a8d14&format=json", {
+    method: 'GET'
+  }).catch(err => {
+    document.getElementById("spotify-status").children[0].innerHTML = 'Not listening to anything on Spotify right now';
+  });
+  let statusJSON = await res.json();
+  
+  console.log(statusJSON);
+  if (statusJSON.recenttracks.track[0]['@attr'] && statusJSON.recenttracks.track[0]['@attr'].nowplaying) {
+    let status = `Listening to ${statusJSON.recenttracks.track[0].name} by ${statusJSON.recenttracks.track[0].artist['#text']} on Spotify`;
     document.getElementById("spotify-status").children[0].innerHTML = status;
   } else {
     document.getElementById("spotify-status").children[0].innerHTML = 'Not listening to anything on Spotify right now';
   }
 }
 
-function getData() {
-  lanyard({
-    userId: "611012357425987615",
-  }).then(displayStatus)
-}
+setInterval(getSpotifyStatus,5000)
 
-setInterval(getData, 3000)
-
-getData();
+getSpotifyStatus();
 
 window.onload = async () => {
   if (localStorage.getItem("playedGame") !== null) {
